040c0b25e9a594b4aefe1e026cda059603e02fb2
mewxz029 <mewlawbreakertv@gmail.com>
2024-10-13 13:32:00 +0700

    Chapter Loop

---
+++ 07_loop/go.mod
@@ -0,0 +1,3 @@
+module github.com/mewxz029/07_loop
+
+go 1.23.2
---
+++ 07_loop/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	// for i := 1; i <= 3; i++ {
+	// 	fmt.Println(i)
+	// }
+
+	// var sum int
+	// for i := 1; i <= 10; i++ {
+	// 	sum += i
+	// }
+	// fmt.Println(sum)
+
+	// For go v1.22+
+	for i := range 3 { // [0, 2]
+		fmt.Println(i)
+	}
+}
